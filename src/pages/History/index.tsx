38230468953c09f1a6fd399a43016e31e085f77b
Neander de Souza <neander.faria@hotmail.com>
2023-06-22 15:14:43 -0300

    Corrigindo listagem do históricoclear

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -36,7 +36,7 @@ export function History() {
                   <td>{cycle.minutesAmount} minutos</td>
 
                   <td>
-                    {formatDistanceToNow(cycle.startDate, {
+                    {formatDistanceToNow(new Date(cycle.startDate), {
                       addSuffix: true,
                       locale: ptBR,
                     })}
